fea9bfe13ee58d4756ec63fd26449529e80b2613
Anthony Godin <gdn.anthony@gmail.com>
2019-09-22 03:44:19 +1200

    feat(issue-1) display images (mock)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { requestImages } from './packages/requesters/images';
 
 function App() {
+  const [images, setImages] = useState(null);
+
+  useEffect(() => {
+    requestImages().then((imgs) => setImages(imgs));
+  });
+
   return (
     <div className="App">
-      IN PROGRESS
+      <h1>Images</h1>
+      {
+        (images === null) ? <div>Loading...</div>
+          : (
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th scope="col">Group</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Version</th>
+                </tr>
+              </thead>
+              <tbody>
+                {images.map((img) => (
+                  <tr key={img.id}>
+                    <td>{img.group}</td>
+                    <td>{img.name}</td>
+                    <td>{img.version}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )
+      }
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './custom.scss';
+import './packages/scss/custom.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
