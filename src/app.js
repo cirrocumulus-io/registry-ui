32086c5fa8252cfd08fd1d179fa9359795337f78
Anthony Godin <gdn.anthony@gmail.com>
2019-10-06 23:40:04 +1300

    initialize project with parcel

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import { requestImages } from './packages/requesters/images';
+
+
+class App extends HTMLElement {
+    constructor() {
+        super();
+        console.log('constructeddddddddd!');
+    }
+
+    connectedCallback() {
+        console.log('connected!');
+        requestImages().then((imgs) => console.log(imgs));
+    }
+
+    disconnectedCallback() {
+        console.log('disconnected!');
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        console.log(`Attribute: ${name} changed!`);
+    }
+
+    adoptedCallback() {
+        console.log('adopted!');
+    }
+}
+
+window.customElements.define('ui-app', App);
