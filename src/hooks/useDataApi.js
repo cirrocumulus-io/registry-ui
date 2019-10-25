6be25fe279df4b03223ab56995010ff8352a238b
Anthony Godin <gdn.anthony@gmail.com>
2019-10-25 09:14:03 +0200

    test new hook behaviour

--- src/hooks/useDataApi.js
+++ src/hooks/useDataApi.js
@@ -1,32 +1,62 @@
 import { useState, useEffect } from 'react';
 
+const initialState = {
+  data: null,
+  isLoading: true,
+  isError: false,
+};
+
+// TODO: put this out of this hook.
+const compareState = (state1, state2) => {
+  if (!state1 && !state2) {
+    return true;
+  }
+
+  // eslint-disable-next-line no-mixed-operators
+  if (!state1 && state2 || state1 && !state2) {
+    return false;
+  }
+
+  return state1.isLoading === state2.isLoading
+      && state1.isError === state2.isError
+      && !state1.data && !state2.data;
+};
+
 export const useDataApi = (request) => {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+  const [state, setState] = useState(initialState);
 
   useEffect(() => {
     let didCancel = false;
     const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
+      if (compareState(initialState, state)) {
+        setState(initialState);
+      }
       try {
         const result = await request();
         if (!didCancel) {
-          setData(result.data);
+          setState((currentState) => ({
+            ...currentState,
+            data: result.data,
+            isLoading: false,
+          }));
         }
       } catch (error) {
         if (!didCancel) {
-          setIsError(true);
+          setState((currentState) => ({
+            ...currentState,
+            isError: true,
+            isLoading: false,
+          }));
         }
       }
-      setIsLoading(false);
     };
+
     fetchData();
 
     return () => {
       didCancel = true;
     };
   }, [request]);
-  return { data, isLoading, isError };
+
+  return state;
 };
